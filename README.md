9be33f1dd4358b1b2e47ff5f3bbb0badd87341a3
Muniandi Baskaran <muniandibaskaran@Muniandis-MacBook-Pro.local>
2025-08-01 11:16:20 +0530

    MCP Server & Gemini Client for Ecommerce data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# mcp-server-data
-Data files and configuration for implementing Model Context Protocol (MCP) servers.
+# mcp-ecommerce-server-data
+Ecommerce Data files exploration for implementing Model Context Protocol (MCP) servers.
+
+# Ecommerce Sample Data
+https://www.kaggle.com/datasets/ayushparwal2026/online-ecommerce?resource=download
+
+# FastMCP 
+https://gofastmcp.com/getting-started/welcome
+
+# 
---
+++ ecommerce-mcp-server/mcp_gemini_client.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+from mcp_use import MCPAgent, MCPClient
+import json
+from langchain_google_genai import ChatGoogleGenerativeAI
+async def main():
+    config = {
+        "mcpServers": {
+            "http": {
+                "url": "http://localhost:8000/sse"
+            }
+        }
+    }
+
+    client = MCPClient.from_dict(config)
+
+    llm = ChatGoogleGenerativeAI(
+        api_key='<api-key>',
+        model="gemini-2.5-flash-preview-05-20",
+        max_retries=2,
+        timeout=300,
+        transport="rest"
+    )
+    # Create agent with the client
+    agent = MCPAgent(llm=llm, client=client, max_steps=30)
+
+    while True:
+        try:
+            user_input = input("\nYou > ")
+            if user_input.lower() in ['exit', 'quit']:
+                print("\nExiting...")
+                break
+            if not user_input:
+                continue
+            print(" Translating your request into a command...")
+            result = await agent.run(user_input)
+            print(f"\nEcommerce Bot: {result}")
+        except (KeyboardInterrupt, EOFError):
+            print("\nExiting...")
+            break
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
